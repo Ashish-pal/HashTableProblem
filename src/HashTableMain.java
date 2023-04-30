db70a04d251e7c264afc00e8e97c9efbbe7ccd34
Ashish Pal <ak987982@gmail.com>
2023-04-30 15:35:56 +0530

    Added UC1 find frequency of words

--- src/HashTableMain.java
+++ src/HashTableMain.java
@@ -1,5 +1,18 @@
 public class HashTableMain {
     public static void main(String[] args) {
         System.out.println("Welcome to Hash table problem solution.");
+        String totalWords = "To be or not to be";
+        String[] words = totalWords.split(" ");
+
+        MyHashTable hashTable = new MyHashTable();
+
+        for (String word : words) {
+            hashTable.add(word);
+        }
+
+        for (String word : words) {
+            int frequency = hashTable.get(word);
+            System.out.println(word + ": " + frequency);
+        }
     }
 }
